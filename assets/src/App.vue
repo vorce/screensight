4347973652b2031f000f43918c01412241c82a63
Joel C <joel@vorce.se>
2018-12-18 23:14:03 +0100

    Add vue and start building the front-end in it

---
+++ assets/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Screensight</h1>
+    </header>
+    <main>
+      <chat></chat>
+    </main>
+  </div>
+</template>
+<script>
+  import Chat from './components/Chat'
+  export default {
+    name: 'app',
+    components: {
+      Chat
+    }
+  }
+</script>
+<style>
+  main {
+    margin-top: 60px;
+  }
+</style>
