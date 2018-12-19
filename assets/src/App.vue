1176d33ce75868a8540b5c4ff637b276e8d96d32
Joel Carlbark <joel.carlbark@meltwater.com>
2018-12-19 12:31:52 +0100

    More UI noodling. Set up grid etc.

--- assets/src/App.vue
+++ assets/src/App.vue
@@ -1,12 +1,26 @@
 <template>
-  <div id="app">
-    <header>
-      <h1>Screensight</h1>
-    </header>
-    <main>
-      <media-share></media-share>
-      <chat></chat>
-    </main>
+
+  <div id="app" class="siimple-content siimple-content--fluid">
+    <div class="siimple-grid">
+      <div class="siimple-grid-row">
+          <div class="siimple-grid-col siimple-grid-col--12">
+            <header>
+              <h1>Screensight</h1>
+            </header>
+          </div>
+      </div>
+
+      <div class="siimple-grid-row">
+        <main>
+        <div class="siimple-grid-col siimple-grid-col--9"><media-share></media-share></div>
+        <div class="siimple-grid-col siimple-grid-col--3"><chat></chat></div>
+        </main>
+      </div>
+    </div>
+
+    <div class="siimple-footer siimple-footer--light siimple-footer--fluid" align="center">
+      <div class="siimple-footer-subtitle">Screensight</div> <a href="#" class="siimple-footer-link siimple-footer-link--inline">Github</a> <a href="#" class="siimple-footer-link siimple-footer-link--inline">Contact</a>
+    </div>
   </div>
 </template>
 <script>
@@ -22,7 +36,4 @@
   }
 </script>
 <style>
-  main {
-    margin-top: 60px;
-  }
 </style>
